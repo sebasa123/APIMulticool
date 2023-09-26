908391a70a6402ab0f428c08a4a4c221787436f5
sebasa123 <sebasariasm123@gmail.com>
2023-09-26 17:47:45 -0600

    Add project files.

---
+++ APIMulticool/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllers();
+        var CnnStrBuilder = new SqlConnectionStringBuilder(builder.Configuration.GetConnectionString("CNNSTR"));
+        string cnnStr = CnnStrBuilder.ConnectionString;
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        var app = builder.Build();
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+        app.UseRouting();
+        app.UseAuthorization();
+        app.MapControllers();
+        app.Run();
+    }
+}
